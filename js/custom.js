89ac170c2d8e39bf43eacfaf0da986d5510e6848
Jens Dieskau <jens@dieskau.pm>
2013-10-08 19:05:43 +0200

    Add slide show for smartphone screenshots

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// Responsiveslides.js
+$("#slider").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 500,
+        timeout: 10000,
+        namespace: "phone-slide"
+});
