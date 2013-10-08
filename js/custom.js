88aa59f590f8c94d99ab73125c3fdfa83ac5e264
Jens Dieskau <jens@dieskau.pm>
2013-10-08 22:08:24 +0200

    Fix green seperator on top

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,27 @@ $("#slider").responsiveSlides({
         timeout: 10000,
         namespace: "phone-slide"
 });
+
+
+$.fn.followTo = function (pos, height) {
+    var $this = this,
+        $window = $(window);
+
+    $window.scroll(function (e) {
+        var top = $window.scrollTop();
+        if (top > pos && top < pos+height) {
+            $("#header").css({
+              'border-bottom': '15px solid #1abc9c'
+            });
+        } else {
+            $("#header").css({
+              'border-bottom': ''
+            });
+        }
+    });
+};
+
+$('#features').followTo(
+  $('#head').outerHeight() - $('#header').outerHeight(),
+  $('#features').outerHeight()
+);
